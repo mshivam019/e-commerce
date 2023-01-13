f230d7bca328fbfd33b9b5c2da910fa9ac67eed7
Shivam Mishra <mshivam019@gmail.com>
2023-01-13 16:52:21 +0530

    fix

--- components/Cart.js
+++ components/Cart.js
@@ -1,7 +1,7 @@
-import React,{useContext,useEffect,useState } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
-import Link from 'next/link';
-import { Store } from "../utils/Store"
+import Link from "next/link";
+import { Store } from "../utils/Store";
 import {
   XIcon,
   PlusCircleIcon,
@@ -9,21 +9,25 @@ import {
 } from "@heroicons/react/outline";
 
 const Cart = ({ isCartOpen, setIsCartOpen }) => {
-  
+  const [first, setFirst] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(first + " this coupon is not valid");
+  };
   const { state, dispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
   const removeItemHandler = (item) => {
-    dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+    dispatch({ type: "CART_REMOVE_ITEM", payload: item });
   };
   const updateCartHandler = async (item) => {
     const existItem = item.quantity;
-    const quantity = existItem + 1 ;
+    const quantity = existItem + 1;
     if (item.countInStock < quantity) {
-      return alert('Sorry. Product is out of stock');
+      return alert("Sorry. Product is out of stock");
     }
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
   };
   return (
     <div
@@ -46,53 +50,71 @@ const Cart = ({ isCartOpen, setIsCartOpen }) => {
         </div>
       ) : (
         <div>
-                          {cartItems.map((item) => (
-                            <div key={item._id}>
-      <hr className="my-3" />
-      <div className="flex justify-between mt-6">
-        <div className="flex">
-          <Image
-            src={item.image}
-            height={80}
-            width={80}
-            style={{objectFit:"cover"}}
-            className="rounded"
-            alt={item.name}
-          />
-          <div className="mx-3">
-            <h3 className="text-sm text-gray-600">{item.name}</h3>
-            <div className="flex items-center mt-2">
-              <button className="text-gray-500 focus:outline-none focus:text-gray-600" onClick={() => updateCartHandler(item)}>
-                <PlusCircleIcon className="h-5 w-5" />
-              </button>
-              <span className="text-gray-700 mx-2">{item.quantity} </span>
-              <button className="text-gray-500 focus:outline-none focus:text-gray-600" onClick={() => removeItemHandler(item)}>
-                <XIcon className="h-5 w-5" />
-              </button>
+          {cartItems.map((item) => (
+            <div key={item._id}>
+              <hr className="my-3" />
+              <div className="flex justify-between mt-6">
+                <div className="flex">
+                  <Image
+                    src={item.image}
+                    height={80}
+                    width={80}
+                    style={{ objectFit: "cover" }}
+                    className="rounded"
+                    alt={item.name}
+                  />
+                  <div className="mx-3">
+                    <h3 className="text-sm text-gray-600">{item.name}</h3>
+                    <div className="flex items-center mt-2">
+                      <button
+                        className="text-gray-500 focus:outline-none focus:text-gray-600"
+                        onClick={() => updateCartHandler(item)}
+                      >
+                        <PlusCircleIcon className="h-5 w-5" />
+                      </button>
+                      <span className="text-gray-700 mx-2">
+                        {item.quantity}{" "}
+                      </span>
+                      <button
+                        className="text-gray-500 focus:outline-none focus:text-gray-600"
+                        onClick={() => removeItemHandler(item)}
+                      >
+                        <XIcon className="h-5 w-5" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <span className="text-gray-600">${item.price}</span>
+              </div>
             </div>
+          ))}
+          <div className="mt-8">
+            <form
+              className="flex items-center justify-center"
+              onSubmit={handleSubmit}
+            >
+              <input
+                className="form-input w-48"
+                type="text"
+                id="first"
+                name="first"
+                placeholder="Add promocode"
+                value={first}
+                onChange={(event) => setFirst(event.target.value)}
+              />
+              <button
+                type="submit"
+                className="ml-3 flex items-center px-3 py-2 bg-green-600 text-white text-sm uppercase font-medium rounded hover:bg-green-500 focus:outline-none focus:bg-green-500"
+              >
+                <span>Apply</span>
+              </button>
+            </form>
           </div>
+          <a className="flex items-center justify-center mt-4 px-3 py-2 bg-green-600 text-white text-sm uppercase font-medium rounded hover:bg-green-500 focus:outline-none focus:bg-green-500 cursor-pointer">
+            <span>Check out</span>
+            <ArrowNarrowRightIcon className="w-5 h-5" />
+          </a>
         </div>
-        <span className="text-gray-600">${item.price}</span>
-      </div>
-      </div>
-                     ))}
-                     <div className="mt-8">
-        <form className="flex items-center justify-center">
-          <input
-            className="form-input w-48"
-            type="text"
-            placeholder="Add promocode"
-          />
-          <button className="ml-3 flex items-center px-3 py-2 bg-green-600 text-white text-sm uppercase font-medium rounded hover:bg-green-500 focus:outline-none focus:bg-green-500">
-            <span>Apply</span>
-          </button>
-        </form>
-      </div>
-      <a className="flex items-center justify-center mt-4 px-3 py-2 bg-green-600 text-white text-sm uppercase font-medium rounded hover:bg-green-500 focus:outline-none focus:bg-green-500 cursor-pointer">
-        <span>Check out</span>
-        <ArrowNarrowRightIcon className="w-5 h-5" />
-      </a>
-      </div>
       )}
     </div>
   );
