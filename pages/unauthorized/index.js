ccf504df503c80586c9e2209661e0f1481ee3421
Shivam Mishra <mshivam019@gmail.com>
2023-01-14 18:08:04 +0530

    fix

--- pages/unauthorized/index.js
+++ pages/unauthorized/index.js
@@ -1,15 +1,28 @@
 import { useRouter } from "next/router";
 import React from "react";
-import Layout from "../components/Layout";
+import Head from "next/head";
+import Pheader from "../../components/Pheader";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
 
 export default function Unauthorized() {
   const router = useRouter();
   const { message } = router.query;
 
   return (
-    <Layout title="Unauthorized Page">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <Head>
+      <title>E-commerce- Shipping</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="bg-white w-full min-h-screen">
+      <Pheader />
+      <Container>
       <h1 className="text-xl">Access Denied</h1>
       {message && <div className="mb-4 text-red-500">{message}</div>}
-    </Layout>
+      </Container>
+        <Footer />
+      </div>
+    </div>
   );
 }
